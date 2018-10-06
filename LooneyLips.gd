1bdb899056a748b6c142d00c6c2814e0b0f10210
PakoSt <kokotekilata@gmail.com>
2018-10-06 20:17:33 +0100

    S2L11 - Using the Player's input
    Optimize the intro and check_player_words_lenght to use less steps

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -5,11 +5,10 @@ var prompt = ["thing", "thing", "feeling", "feeling", "action", "feeling"]
 
 var story = "Once upon a time %s ate a %s and felt very %s. He was quite %s to %s very %s"
 
-var intro = "Welcome to Looney Lips! \nWant to play a game? \n \n"
+var intro = "Welcome to Looney Lips! \nWant to play a game? \n \n Can I have a " + prompt[player_words.size()] + ", please?"
 
 func _ready():
 	displayIntro()
-	check_player_words_lenght()
 	$Blackboard/LineEdit.grab_focus()
 
 func displayIntro():
@@ -24,14 +23,13 @@ func _on_LineEdit_text_entered(new_text):
 
 func check_player_words_lenght():
 	if(player_words.size() < prompt.size()):
-		$Blackboard/StoryText.text += ("Can I have a " + prompt[player_words.size()] + ", please?")
+		$Blackboard/StoryText.text = ("Can I have a " + prompt[player_words.size()] + ", please?")
 	else:
 		tellStory()
 
 func processPlayerInput():
 	player_words.append($Blackboard/LineEdit.text)
 	$Blackboard/LineEdit.text = ""
-	$Blackboard/StoryText.text = ""
 	check_player_words_lenght()
 
 func tellStory():
