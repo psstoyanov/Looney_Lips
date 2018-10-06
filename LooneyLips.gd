d23bb65ac963088f1d17502bf11b871729b58bea
PakoSt <kokotekilata@gmail.com>
2018-10-06 16:24:45 +0100

    S2L8 - Expand on the story
    Add more prompt entries and make the story bigger

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _ready():
-	var promt = ["Pako", "pizza", "full"]
-	var story = "Once upon a time %s ate a %s and felt very %s"
+	var promt = ["Pako", "pizza", "full","sleepy","jump","bloated"]
+	var story = "Once upon a time %s ate a %s and felt very %s. He was quite %s to %s very %s"
 	
 	print(story % promt)
