6ddea1334af27ab1d8db2f366290cc9292f42544
PakoSt <kokotekilata@gmail.com>
2018-10-06 17:59:10 +0100

    S2L9 - RichTextLabel
    Add background
    Add Story Text Node to show the players
    Use DynamicFont for the Story Text

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -4,4 +4,5 @@ func _ready():
 	var promt = ["Pako", "pizza", "full","sleepy","jump","bloated"]
 	var story = "Once upon a time %s ate a %s and felt very %s. He was quite %s to %s very %s"
 	
+	$Blackboard/StoryText.text = story % promt
 	print(story % promt)
