02e7cd89ff05f60918036848de1739d6795fe3c3
PakoSt <kokotekilata@gmail.com>
2018-10-06 19:17:57 +0100

    S2L10 - Text From Player
    Add LineEdit field for player input
    Use Signal to pass the player input to the LooneyLips script

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -4,5 +4,8 @@ func _ready():
 	var promt = ["Pako", "pizza", "full","sleepy","jump","bloated"]
 	var story = "Once upon a time %s ate a %s and felt very %s. He was quite %s to %s very %s"
 	
-	$Blackboard/StoryText.bbcode_text = story % promt
+	$Blackboard/StoryText.text = story % promt
 	print(story % promt)
+
+func _on_LineEdit_text_entered(new_text):
+	$Blackboard/StoryText.text = new_text
