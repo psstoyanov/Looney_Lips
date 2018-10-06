5c26716d314339f7b7de9e2e50c6c81f4e654cc8
PakoSt <kokotekilata@gmail.com>
2018-10-06 20:13:18 +0100

    S2L11 - Using the Player's input
    Add function to process the player input
    Use "if" statement to prompt for new words or display the story
    Add intro

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -1,22 +1,38 @@
 extends Node2D
 
-func _ready():
-	var promt = ["Pako", "pizza", "full","sleepy","jump","bloated"]
-	var story = "Once upon a time %s ate a %s and felt very %s. He was quite %s to %s very %s"
-	
-	$Blackboard/StoryText.text = story % promt
-	print(story % promt)
+var player_words = [] # The player words to use for the story
+var prompt = ["thing", "thing", "feeling", "feeling", "action", "feeling"]
 
+var story = "Once upon a time %s ate a %s and felt very %s. He was quite %s to %s very %s"
+
+var intro = "Welcome to Looney Lips! \nWant to play a game? \n \n"
+
+func _ready():
+	displayIntro()
+	check_player_words_lenght()
+	$Blackboard/LineEdit.grab_focus()
+
+func displayIntro():
+	$Blackboard/StoryText.text = intro
 
 func _on_TextureButton_pressed():
-	var new_text = $Blackboard/LineEdit.text
-	$Blackboard/StoryText.text = new_text
-	ClearPlayerInput()
+	processPlayerInput()
 
 func _on_LineEdit_text_entered(new_text):
-	$Blackboard/StoryText.text = new_text	
-	ClearPlayerInput()
+	processPlayerInput()
 
-func ClearPlayerInput():
+
+func check_player_words_lenght():
+	if(player_words.size() < prompt.size()):
+		$Blackboard/StoryText.text += ("Can I have a " + prompt[player_words.size()] + ", please?")
+	else:
+		tellStory()
+
+func processPlayerInput():
+	player_words.append($Blackboard/LineEdit.text)
 	$Blackboard/LineEdit.text = ""
+	$Blackboard/StoryText.text = ""
+	check_player_words_lenght()
 
+func tellStory():
+	$Blackboard/StoryText.text = story % player_words
