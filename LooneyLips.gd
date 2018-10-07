cd57f8e5f3cc5162d104af731a12693bf602cac2
PakoSt <kokotekilata@gmail.com>
2018-10-07 23:08:49 +0100

    S2L13 - Dictionaries - Adding a Story Template
    - Convert "story" and "prompt" into a dictionary template
    - Create an array of story dictionaries
    - Refactor to use the new dictionaries instead
    - Choose a story at random
    
    Fix:
    - Add a word prompt when calling the intro
    Refactor:
    - Use a new word prompt variable instead of hard coded string

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -5,16 +5,35 @@ var prompt = ["thing", "thing", "feeling", "feeling", "action", "feeling"]
 
 var story = "Once upon a time %s ate a %s and felt very %s. He was quite %s to %s very %s"
 
-var intro = "Welcome to Looney Lips! \nWant to play a game? \n \nCan I have a " + prompt[player_words.size()] + ", please?"
+var template = [{
+		"prompt" : ["thing", "adjective", "feeling", "feeling", "action", "feeling"],
+		"story" : "Once upon a time %s ate a %s and felt very %s. He was quite %s to %s very %s"
+		},{
+		"prompt" : ["noun", "adjective", "verb"],
+		"story" : "Once upon a time %s bough %s and hastily %s."
+		}]
+
+var current_story
+
+var intro = "Welcome to Looney Lips! \nWant to play a game? \n \n"
+var new_word_promt = "Can I have a %s, please?"
 var play_again_txt = "\n\n\n\nDo you want to play again?"
 var button_again = "Again!"
 
 func _ready():
+	prepare_game()
+
+func prepare_game():
+	choose_random_story()
 	display_intro()
 	$Blackboard/LineEdit.grab_focus()
 
+func choose_random_story():
+	randomize()
+	current_story = template[randi() % template.size()]
+
 func display_intro():
-	$Blackboard/StoryText.text = intro
+	$Blackboard/StoryText.text = intro + (new_word_promt % current_story.prompt[player_words.size()])
 
 func _on_TextureButton_pressed():
 	check_player_words_lenght()
@@ -23,7 +42,7 @@ func _on_LineEdit_text_entered(new_text):
 	check_player_words_lenght()
 
 func check_player_words_lenght():
-	if(player_words.size() < prompt.size()):
+	if(player_words.size() < current_story.prompt.size()):
 		add_player_word_to_story()
 	elif(check_if_game_has_finished()):
 		tell_story()
@@ -31,12 +50,12 @@ func check_player_words_lenght():
 		restart_the_game()
 
 func add_player_word_to_story():
-	$Blackboard/StoryText.text = ("Can I have a " + prompt[player_words.size()] + ", please?")
+	$Blackboard/StoryText.text = (new_word_promt % current_story.prompt[player_words.size()])
 	player_words.append($Blackboard/LineEdit.text)
 	$Blackboard/LineEdit.text = ""
 
 func tell_story():
-	$Blackboard/StoryText.text = story % player_words
+	$Blackboard/StoryText.text = current_story.story % player_words
 	show_play_again()
 
 func show_play_again():
