e9767cc74087e5c7e2d0ca2a9a62284ba8b86a03
PakoSt <kokotekilata@gmail.com>
2018-10-07 22:31:19 +0100

    S2L12 - queue_free and reload_current_scene
    - Display "Play again" message
    - Restart the scene if the story has been told
    - Refactor the player input and end game state

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -5,32 +5,47 @@ var prompt = ["thing", "thing", "feeling", "feeling", "action", "feeling"]
 
 var story = "Once upon a time %s ate a %s and felt very %s. He was quite %s to %s very %s"
 
-var intro = "Welcome to Looney Lips! \nWant to play a game? \n \n Can I have a " + prompt[player_words.size()] + ", please?"
+var intro = "Welcome to Looney Lips! \nWant to play a game? \n \nCan I have a " + prompt[player_words.size()] + ", please?"
+var play_again_txt = "\n\n\n\nDo you want to play again?"
+var button_again = "Again!"
 
 func _ready():
-	displayIntro()
+	display_intro()
 	$Blackboard/LineEdit.grab_focus()
 
-func displayIntro():
+func display_intro():
 	$Blackboard/StoryText.text = intro
 
 func _on_TextureButton_pressed():
-	processPlayerInput()
+	check_player_words_lenght()
 
 func _on_LineEdit_text_entered(new_text):
-	processPlayerInput()
-
+	check_player_words_lenght()
 
 func check_player_words_lenght():
 	if(player_words.size() < prompt.size()):
-		$Blackboard/StoryText.text = ("Can I have a " + prompt[player_words.size()] + ", please?")
+		add_player_word_to_story()
+	elif(check_if_game_has_finished()):
+		tell_story()
 	else:
-		tellStory()
+		restart_the_game()
 
-func processPlayerInput():
+func add_player_word_to_story():
+	$Blackboard/StoryText.text = ("Can I have a " + prompt[player_words.size()] + ", please?")
 	player_words.append($Blackboard/LineEdit.text)
 	$Blackboard/LineEdit.text = ""
-	check_player_words_lenght()
 
-func tellStory():
+func tell_story():
 	$Blackboard/StoryText.text = story % player_words
+	show_play_again()
+
+func show_play_again():
+	$Blackboard/StoryText.text += play_again_txt
+	$Blackboard/TextureButton/RichTextLabel.text = button_again
+	$Blackboard/LineEdit.queue_free() # Remove the LineEdit
+
+func check_if_game_has_finished():
+	return $Blackboard/LineEdit # If the player input still exists, tell the story first
+
+func restart_the_game():
+	get_tree().reload_current_scene()
