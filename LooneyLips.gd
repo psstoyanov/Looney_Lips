59d5224f177891437e399ff00eb216f26afdc0b8
PakoSt <kokotekilata@gmail.com>
2018-10-13 22:20:38 +0100

    Add nodes for story and play
    Make LineEdit and NewWordButton work only during gameplay
    Use Story and Play Button nodes to handle the start/end states

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -10,7 +10,6 @@ func _ready():
 	choose_random_story()
 	get_other_strings()
 	display_intro()
-	$Blackboard/LineEdit.grab_focus()
 
 func get_other_strings():
 	var other_strings = get_from_json("other_strings.json")
@@ -31,13 +30,31 @@ func get_from_json(filename):
 	return data
 
 func display_intro():
-	$Blackboard/StoryText.text = strings.intro + ask_for_new_word()
+	$Blackboard/StoryText.show()
+	$Blackboard/StartGameButton.show()
+	$Blackboard/StoryText.text = strings.intro
+	$Blackboard/StartGameButton/RichTextLabel.text = strings.play_game
+	$Blackboard/NewWordButton.hide()
+	$Blackboard/LineEdit.hide()
+
+func _on_StartGameButton_pressed():
+	if(player_words.size() < current_story.prompt.size()):
+		hide_intro_story_and_start_button()
+		print(ask_for_new_word())
+	else:
+		restart_the_game()
+
+func hide_intro_story_and_start_button():
+	$Blackboard/StoryText.hide()
+	$Blackboard/StartGameButton.hide()
+	$Blackboard/LineEdit.show()
+	$Blackboard/NewWordButton.show()
 	$Blackboard/LineEdit.placeholder_text = ask_for_new_word()
 
 func ask_for_new_word():
 	return strings.new_word_prompt % current_story.prompt[player_words.size()]
 
-func _on_TextureButton_pressed():
+func _on_NewWordButton_pressed():
 	check_player_words_lenght()
 
 func _on_LineEdit_text_entered(new_text):
@@ -48,26 +65,29 @@ func check_player_words_lenght():
 		add_player_word_to_story()
 	elif(check_if_game_has_finished()):
 		tell_story()
-	else:
-		restart_the_game()
 
 func add_player_word_to_story():
-	$Blackboard/StoryText.text = ask_for_new_word()
 	$Blackboard/LineEdit.placeholder_text = ask_for_new_word()
 	player_words.append($Blackboard/LineEdit.text)
 	$Blackboard/LineEdit.text = ""
 
 func tell_story():
+	$Blackboard/StoryText.show()
 	$Blackboard/StoryText.text = current_story.story % player_words
 	show_play_again()
 
 func show_play_again():
 	$Blackboard/StoryText.text += strings.play_again_txt
-	$Blackboard/TextureButton/RichTextLabel.text = strings.button_again
+	$Blackboard/StartGameButton.show()
+	$Blackboard/StartGameButton/RichTextLabel.text = strings.button_again
 	$Blackboard/LineEdit.queue_free() # Remove the LineEdit
+	$Blackboard/NewWordButton.queue_free() # Remove the NewWordButton
 
 func check_if_game_has_finished():
 	return $Blackboard/LineEdit # If the player input still exists, tell the story first
 
 func restart_the_game():
 	get_tree().reload_current_scene()
+
+
+
